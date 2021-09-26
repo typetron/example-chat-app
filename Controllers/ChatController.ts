ed0a4ffd06ea81e2fba4de66753458b0748c4a30
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2021-09-26 22:07:52 +0300

    initial commit

--- Controllers/ChatController.ts
+++ Controllers/ChatController.ts
@@ -5,29 +5,6 @@ import { User } from 'App/Entities/User'
 import { SearchResults } from 'App/Models/SearchResults'
 import { AuthMiddleware } from '@Typetron/Framework/Middleware'
 
-/*
-    + add route parameters on websockets so we can use Route-Entity binding in websockets endpoints
-    + fix issue with loading websockets after server refresh (needs to be really smooth). It doesn't work anymore if the server is restarted
-    + fix TODOs
-    + use @Middleware(AuthMiddleware) (test it with a auth token expiration time of 10s)
-    + separate controllers per entities
-    + add "remove" user from room
-    + add "user invite" to rooms used as groups
-    + make the Websockets server to not start when running tests
-    + find a way to not run the websockets in test or when run from the `typetron` command in CLI
-    + add "create room" button
-    + add "remove room" button
-    + use a class to inject the Websocket instead of using @Inject('socket')
-    + subscribe the "auth user" to each room as a topic with this format: 'room.{id}'
-    + add decorators for @OnOpen and @OnClose websocket events
-    + fix issue where the Request object is not correct if multiple WS requests are made
-    - use Typetron forms in React
-    - add unit tests
-    - have better naming for events and subscriptions
-    + check why we can't have multiple users logged in
-    - try to find an easier way to save images through websockets
-*/
-
 @Controller()
 export class ChatController {
 
